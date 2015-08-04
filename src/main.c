0a6e2ecbecda176638c07922cfa0bfde634c8457
Isabella <imoreira@salesforce.com>
2015-08-03 17:50:39 -0700

    added seconds only to display with wrist flick for 3 secs

--- src/main.c
+++ src/main.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dithered_rects.h>
+#include <stdbool.h>
+
+#define DURATION_OF_SECONDS_AFTER_TAP 3
   
 static Window *s_main_window;
 static TextLayer *s_hr_layer;
@@ -13,97 +15,51 @@ static TextLayer *s_battery_layer;
 static TextLayer *s_color_name_layer;
 static Window *windowMain;
 
-static uint8_t supportedColors[60] = {
-  GColorMalachiteARGB8,
-  GColorScreaminGreenARGB8,
-  GColorGreenARGB8,
-  GColorBrightGreenARGB8,
-  GColorSpringBudARGB8,
-  GColorInchwormARGB8,
-  GColorMediumAquamarineARGB8,
-  GColorMediumSpringGreenARGB8,
-  GColorMintGreenARGB8,
-  GColorCelesteARGB8,
-  GColorElectricBlueARGB8,
-  GColorCyanARGB8,
-  GColorTiffanyBlueARGB8,
-  GColorCadetBlueARGB8,
-  GColorMidnightGreenARGB8,
-  GColorDarkGreenARGB8,
-  GColorKellyGreenARGB8,
-  GColorIslamicGreenARGB8,
-  GColorJaegerGreenARGB8,
-  GColorMayGreenARGB8,
-  GColorBrassARGB8,
-  GColorArmyGreenARGB8,
-  GColorLimerickARGB8,
-  GColorPastelYellowARGB8,
-  GColorIcterineARGB8,
-  GColorYellowARGB8,
-  GColorWindsorTanARGB8,
-  GColorChromeYellowARGB8,
-  GColorRajahARGB8,
-  GColorOrangeARGB8,
-  GColorRedARGB8,
-  GColorFollyARGB8,
-  GColorSunsetOrangeARGB8,
-  GColorMelonARGB8,
-  GColorRichBrilliantLavenderARGB8,
-  GColorBrilliantRoseARGB8,
-  GColorShockingPinkARGB8,
-  GColorMagentaARGB8,
-  GColorFashionMagentaARGB8,
-  GColorRoseValeARGB8,
-  GColorBulgarianRoseARGB8,
-  GColorDarkCandyAppleRedARGB8,
-  GColorJazzberryJamARGB8,
-  GColorPurpleARGB8,
-  GColorVividVioletARGB8,
-  GColorLavenderIndigoARGB8,
-  GColorPurpureusARGB8,
-  GColorBabyBlueEyesARGB8,
-  GColorLibertyARGB8,
-  GColorIndigoARGB8,
-  GColorImperialPurpleARGB8,
-  GColorOxfordBlueARGB8,
-  GColorDukeBlueARGB8,
-  GColorBlueARGB8,
-  GColorBlueMoonARGB8,
-  GColorElectricUltramarineARGB8,
-  GColorVeryLightBlueARGB8,
-  GColorPictonBlueARGB8,
-  GColorVividCeruleanARGB8,
-  GColorCobaltBlueARGB8
-};
+bool isShowingSeconds = false;
+time_t timeOfLastTap = 0;
+
 
 int r_val = 0;
 int g_val = 0;
 int b_val = 0;
 
-static void layer_update_callback(Layer *layer, GContext* ctx) {
-  
-  GRect bounds = layer_get_bounds(layer);
-  
-  draw_dithered_rect_from_RGB(ctx, bounds, r_val, g_val, b_val);
-}
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
 
-static void battery_handler(BatteryChargeState new_state) {
-  // Write to buffer and display
-  static char s_battery_buffer[] = "00%";
-  snprintf(s_battery_buffer, sizeof(s_battery_buffer), "%d%%", new_state.charge_percent);
-  text_layer_set_text(s_battery_layer, s_battery_buffer);
-}
+  // Create a long-lived buffer
+  static char bufferHr[] = "#00";
+  static char bufferMin[] = "00";
+  static char bufferDate[] = "00.00";
 
-float calcLuminance(GColor color) {
-      int r = color.r;
-      int g = color.g;
-      int b = color.b;
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    //Use 2h hour format
+    strftime(bufferHr, sizeof("#00"), "#%H", tick_time);
+  } else {
+    //Use 12 hour format
+    strftime(bufferHr, sizeof("#00"), "#%I", tick_time);
+  }
   
-      //printf("%d, %d, %d\t", r, g, b);
-      //printf("%lf", (r+r+b+g+g+g)/6.0);
+  strftime(bufferMin, sizeof("00"), "%M", tick_time);
+  strftime(bufferDate, sizeof("00.00"), "%m.%d", tick_time);
 
-      //return (r*0.299f + g*0.587f + b*0.114f) / 256.0;
-      return (r+r+b+g+g+g)/6.0;
+  // Display this time on the TextLayer
+  text_layer_set_text(s_hr_layer, bufferHr);
+  text_layer_set_text(s_min_layer, bufferMin);
+  text_layer_set_text(s_date_layer, bufferDate);
+
+  static char bufferSec[3];
+  if (isShowingSeconds) {
+    // isShowingSeconds is toggled by a watch bump to save battery.
+    strftime(bufferSec, sizeof("00"), "%S", tick_time);
+    text_layer_set_text(s_sec_layer, bufferSec);
+  } else {
+    // Clear out the seconds field.
+    bufferSec[0] = 0;
+    text_layer_set_text(s_sec_layer, bufferSec);
+  }
 }
 
 static void update_color(struct tm *tick_time) {
@@ -121,11 +77,6 @@ static void update_color(struct tm *tick_time) {
   r_val = (hex[0] - '0')*16+(hex[1] - '0');
   g_val = (hex[2] - '0')*16+(hex[3] - '0');
   b_val = (hex[4] - '0')*16+(hex[5] - '0');
-  
-  printf("TYLER'S NUM: %d %d %d", r_val, g_val, b_val);
-
-  
-  //draw_dithered_rect_from_RGB
 
   // Update main bg color
   window_set_background_color(windowMain, GColorFromRGB(r_val,g_val,b_val));
@@ -133,59 +84,46 @@ static void update_color(struct tm *tick_time) {
   // Update battery and date font colors to match main bg
   text_layer_set_text_color(s_date_layer, GColorFromRGB(r_val,g_val,b_val));
   text_layer_set_text_color(s_battery_layer, GColorFromRGB(r_val,g_val,b_val));
-
-  
-  // Update accent colors depending on luminance
-  if( calcLuminance(GColorFromRGB(r_val,g_val,b_val))>= 1.9) {
-    // Bright main color
-    text_layer_set_text_color(s_hr_layer, GColorDarkGray);
-    text_layer_set_text_color(s_min_layer, GColorDarkGray);
-    text_layer_set_text_color(s_sec_layer, GColorDarkGray);
-    text_layer_set_background_color(s_date_layer, GColorDarkGray);    
-  } else {
-    text_layer_set_text_color(s_hr_layer, GColorWhite);
-    text_layer_set_text_color(s_min_layer, GColorWhite);
-    text_layer_set_text_color(s_sec_layer, GColorWhite);
-    text_layer_set_background_color(s_date_layer, GColorWhite);
-  }
 }
 
-static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
-
-  // Create a long-lived buffer
-  static char bufferHr[] = "#00";
-  static char bufferMin[] = "00";
-  static char bufferSec[] = "00";
-  static char bufferDate[] = "00.00";
-
-  // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-    //Use 2h hour format
-    strftime(bufferHr, sizeof("#00"), "#%H", tick_time);
-  } else {
-    //Use 12 hour format
-    strftime(bufferHr, sizeof("#00"), "#%I", tick_time);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  if(isShowingSeconds && (difftime(time(NULL), timeOfLastTap) > DURATION_OF_SECONDS_AFTER_TAP)) {
+    isShowingSeconds = false;
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   }
-  
-  strftime(bufferMin, sizeof("00"), "%M", tick_time);
-  strftime(bufferSec, sizeof("00"), "%S", tick_time);
-  strftime(bufferDate, sizeof("00.00"), "%m.%d", tick_time);
 
-  // Display this time on the TextLayer
-  text_layer_set_text(s_hr_layer, bufferHr);
-  text_layer_set_text(s_min_layer, bufferMin);
-  text_layer_set_text(s_sec_layer, bufferSec);
-  text_layer_set_text(s_date_layer, bufferDate);
+  update_time();
+  
+  update_color(tick_time);
+}
+
+static void battery_handler(BatteryChargeState new_state) {
+  // Write to buffer and display
+  static char s_battery_buffer[] = "00%";
+  snprintf(s_battery_buffer, sizeof(s_battery_buffer), "%d%%", new_state.charge_percent);
+  text_layer_set_text(s_battery_layer, s_battery_buffer);
+}
+
+
+static void tap_handler(AccelAxisType axis, int32_t direction) {
+  // Save time of last tap.
+  timeOfLastTap = time(NULL);
+  
+  if(!isShowingSeconds) {
+    // Switch to the second_unit timer subscription
+    isShowingSeconds = true;
+      
+    // Immediatley update the time so our tap looks responsive
+    struct tm *tick_time = localtime(&timeOfLastTap);
+    update_time(tick_time);
+
+    // Resubscribe to the tick timer at every second
+    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  }
 }
 
 static void main_window_load(Window *window) {
   windowMain = window;
-//   Layer *window_layer = window_get_root_layer(windowMain);
-//   layer_set_update_proc(window_layer, layer_update_callback);
-  
   
   // Create time TextLayer
   s_hr_layer = text_layer_create(GRect(0, 10, 126, 50));
@@ -201,7 +139,7 @@ static void main_window_load(Window *window) {
   s_sec_layer = text_layer_create(GRect(0, 80, 155, 50));
   text_layer_set_background_color(s_sec_layer, GColorClear);
   text_layer_set_text_color(s_sec_layer, GColorBlack);
-  text_layer_set_text(s_sec_layer, "00");
+  text_layer_set_text(s_sec_layer, "");
   
   s_date_layer = text_layer_create(GRect(0, 145, 144, 25));
   text_layer_set_background_color(s_date_layer, GColorWhite);
@@ -259,12 +197,6 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_date_layer);
   text_layer_destroy(s_battery_layer);
 }
-
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
-  
-  update_color(tick_time);
-}
   
 static void init() {
   // Create main Window element and assign to pointer
@@ -280,31 +212,22 @@ static void init() {
   window_stack_push(s_main_window, true);
   
   // Register with TickTimerService
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   
   // Subscribe to the Battery State Service
   battery_state_service_subscribe(battery_handler);
 
-
-  // Testing things
-  GColor DARK_COLOR = (GColor)GColorMayGreenARGB8;
-
-  int r = DARK_COLOR.r;
-  int g = DARK_COLOR.g;
-  int b = DARK_COLOR.b;
-  
-  printf("COLOR: %d, %d, %d\t", r, g, b);
-
-  if((((r+r+b+g+g+g)/6.0) >= 1.5)) {
-    printf("LIGHT");
-  } else {
-    printf("DARK");
-  }
+  // Subscribe to gestures
+  accel_tap_service_subscribe(tap_handler);
 }
 
 static void deinit() {
   // Destroy Window
   window_destroy(s_main_window);
+
+  tick_timer_service_unsubscribe();
+  battery_state_service_unsubscribe();
+  accel_tap_service_unsubscribe();
 }
 
 int main(void) {
